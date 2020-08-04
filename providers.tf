f8c970baf5794f23c6830a8e7137d18e8e0af9c3
Shubhankar <shubhankar.kalra@successive.tech>
2020-08-04 23:36:18 +0530

    Helm v3 Migration

--- providers.tf
+++ providers.tf
@@ -1,8 +1,8 @@
 provider "helm" {
     
    kubernetes {
-    host     = "https://35.192.199.8"
-    config_path = "~/.kube/config"
+    host = "https://240D2B04916CFB6E91311591E0BEBE0F.sk1.us-west-2.eks.amazonaws.com"
+    # config_path = "~/.kube/config"
   
     }
   }
