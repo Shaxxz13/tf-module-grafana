e4d42993526d1250d0bcd69d379d03c288b83fef
dhirwanashish <63767433+dhirwanashish@users.noreply.github.com>
2020-08-09 22:26:07 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,8 +1,9 @@
-provider "helm" {
+
+# provider "helm" {
     
-   kubernetes {
-    host = "https://240D2B04916CFB6E91311591E0BEBE0F.sk1.us-west-2.eks.amazonaws.com"
-    # config_path = "~/.kube/config"
+#    kubernetes {
+#     host = "https://E0CD00B9939D3C247AC3A332DDF9B303.gr7.us-east-1.eks.amazonaws.com"
+#     config_path = "~/.kube/config"
   
-    }
-  }
+#     }
+#   }
